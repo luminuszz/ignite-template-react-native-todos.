b430dc1cb0fdedd4d1421c742bb1e125e563ac90
Luminuszz <luminuszz43@gmail.com>
2021-06-08 11:57:57 -0300

    finish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Header } from '../components/Header';
-import { MyTasksList } from '../components/MyTasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { MyTasksList } from "../components/MyTasksList";
+import { TodoInput } from "../components/TodoInput";
 
 interface Task {
   id: number;
@@ -11,18 +11,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    const newTask: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    if (newTaskTitle) {
+      setTasks((old) => [...old, newTask]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const foundedTaskIndex = tasks.findIndex((task) => task.id === id);
+
+    if (foundedTaskIndex >= 0) {
+      const copyTasks = [...tasks];
+
+      copyTasks[foundedTaskIndex].done = true;
+
+      setTasks(copyTasks);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const verifyTasksExists = tasks.find((task) => task.id === id);
+
+    if (verifyTasksExists) {
+      setTasks((old) => old.filter((task) => task.id !== id));
+    }
   }
 
   return (
@@ -31,11 +51,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
